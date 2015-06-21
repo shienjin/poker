bba02264bf49b84a3285e36bd5dddfb8ade5db87
Shien Jin Ong <shienjin@gmail.com>
2015-06-21 17:10:52 +0800

    Add test cases

--- poker.py
+++ poker.py
@@ -19,3 +19,19 @@ def card_rank(hand):
     if ranks == [14, 5, 4, 3, 2]:
         ranks = [5, 4, 3, 2, 1]
     return ranks
+
+
+def tests():
+    "Test cases"
+    straight_flush = "5C 6C 7C 8C 9C".split()
+    four_kind = "AD AS AC AH 8C".split()
+    full_house = "KH KC KS TH TC".split()
+    flush = "QC 6C 4C 3C 8C".split()
+    straight = "2S 3C 4S 5H 6H".split()
+    three_kind = "JS JH JC 5C 8D".split()
+    two_pair = "10S 10H 6S 6C KC".split()
+    one_pair = "AH AC 8D 6H 2S".split()
+    high_card = "KH JS 10C 6H 3D".split()
+    assert poker([straight_flush, four_kind, full_house]) == straight_flush
+    assert poker([flush, straight, three_kind]) == flush
+    assert poker([two_pair, one_pair, high_card]) == two_pair
