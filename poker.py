54cb96bad438f24b9e2992347b89ebc1ce39afe9
Shien Jin Ong <shienjin@gmail.com>
2015-06-21 17:01:38 +0800

    Add card_rank function

--- poker.py
+++ poker.py
@@ -7,3 +7,15 @@ def poker(hands):
 def hand_rank(hand):
     "Returns the hand ranking: hand_rank(hand) => ranking"
     return
+
+
+def card_rank(hand):
+    """
+    Returns card rankings in reverse sorted order. Note: A = 14 or 1.
+    card_rank(hand) => [rank_1, rank_2, ...]
+    """
+    ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
+    ranks.sort(reverse=True)
+    if ranks == [14, 5, 4, 3, 2]:
+        ranks = [5, 4, 3, 2, 1]
+    return ranks
